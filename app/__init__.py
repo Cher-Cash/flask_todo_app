8b4151009d2ced9598ce1e7dded6b1e96af13671
Kovach Aleksey <kovach.aleksey19@ya.ru>
2025-01-12 18:03:26 +1000

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import Admin
+from flask_migrate import Migrate
+
+from app.extansions import db
+from app.models import Tasks, Users, Category
+
+
+admin_ext = Admin(template_mode='bootstrap3')
+migrate_ext = Migrate()
+load_dotenv()
+
+
+def create_app(testing=False):
+    new_app = Flask(__name__)
+    if testing:
+        new_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    else:
+        new_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+    db.init_app(new_app)
+    migrate_ext.init_app(new_app, db)
+    admin_ext.init_app(new_app)
+
+    @new_app.route('/ping')
+    def init_route():
+        return jsonify({'status':'ok'})
+
+    return new_app
+
+
+class MyModelView(ModelView):
+    column_display_all_relations = True
+    column_hide_backrefs = False
+
+
+admin_ext.add_view(MyModelView(Tasks, db.session))
+admin_ext.add_view(MyModelView(Users, db.session))
+admin_ext.add_view(MyModelView(Category, db.session))
