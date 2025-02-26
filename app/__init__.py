6086c219373e8fdc01689bc2221be78552a27f7c
Cher-Cash <kovach.aleksey19@yandex.ru>
2025-02-26 16:25:27 +1000

    lint fixes

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 from app.extansions import db
 from app.models import Category, Tasks, Users
@@ -18,7 +18,7 @@ load_dotenv()
 def create_app():
     new_app = Flask(__name__)
     new_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
-    new_app.config['CORS_HEADERS'] = 'Content-Type'
+    new_app.config["CORS_HEADERS"] = "Content-Type"
     db.init_app(new_app)
     migrate_ext.init_app(new_app, db)
     admin_ext.init_app(new_app)
